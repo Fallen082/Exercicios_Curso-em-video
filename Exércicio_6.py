c0a8809ab34eed1755717fcf4f8e9a6bc1ccf985
Fallen082 <82683646+Fallen082@users.noreply.github.com>
2022-02-21 09:33:28 -0300

    Update Exércicio_6.py

--- Exércicio_6.py
+++ Exércicio_6.py
@@ -1,13 +1,16 @@
 '''Mostrar o dobro, triplo, Raiz quadrada do número informado'''
 import math
 
+num = input('Digite algum número')
 try:
-    num = int(input('Digite um número')
-          
-    print(f'''
+
+  num = int(num)
+  print(f'''
           Analisando o Valor {num} vi que:
           O dobro deste número é {num * 2}
           O triplo deste número é {num * 3}
-          e sua raiz quadrada é : {sqrt(num)}''')
+          e sua raiz quadrada é : {math.sqrt(num)}
+          ''')
 except:
        print('Alguma informação foi digitada errada :(')
+
