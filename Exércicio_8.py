0383f09f812998e73ff649b62a5a35465548d125
Fallen082 <82683646+Fallen082@users.noreply.github.com>
2022-02-21 10:38:50 -0300

    Update Exércicio_8.py

--- Exércicio_8.py
+++ Exércicio_8.py
@@ -1,1 +1,16 @@
+'''Conversor de medidas'''
 
+try:
+  Metros = int(input('Digite uma distancia em metros:'))
+  print(f'''
+        Quílometro: {Metros / 1000} KM
+        Hectômetro: {Metros / 100} HM
+        Decâmetro : {Metros / 10} DAM
+        Metro     : {Metros} M
+        Decímetro : {Metros * 10} DM
+        Centímetro: {Metros * 100} CM
+        milímetro : {Metros * 1000} MM
+        ''')
+
+except:
+  print('erro')
