66a36c1c7d4cd81031dc354040fca4a5a01c904b
Fallen082 <82683646+Fallen082@users.noreply.github.com>
2022-02-21 09:07:19 -0300

    Update Exércicio_5.py

--- Exércicio_5.py
+++ Exércicio_5.py
@@ -1,3 +1,5 @@
+'''Mostrar o número antecessor e sucessor do valor informado'''
+
 num = int(input('Digite um número:')
 
 print(f'Analisando o valor {num}, descobri que seu antecessor é {num - 1} e que seu sucessor é {num + 1}')          
