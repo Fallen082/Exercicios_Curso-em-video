4dd6f54f02b1e9551a35b835350e9a5b0a5f3924
Fallen082 <82683646+Fallen082@users.noreply.github.com>
2022-03-01 06:48:28 -0300

    Update Exercício_18.py

--- Exercício_18.py
+++ Exercício_18.py
@@ -1,1 +1,24 @@
+"""Calcular Seno Cosseno e Tangente """
 
+def cos_sen_tan(grau=0):
+    """
+    --> Está função irá retornar um pequeno texto mostrando
+     o Seno, Cosseno e a Tangente do angulo em Graus
+    :param grau: o grau do ángulo no formato int()
+    :return: uma str() com as informações colocadas em cima"""
+    from math import cos, tan, sin, radians
+
+    print(f'''
+     Cosseno de {grau}° é : {cos(radians(grau)):.2f}
+        Seno de {grau}° é : {sin(radians(grau)):.2f}
+    Tangente de {grau}° é : {tan(radians(grau)):.2f}''')
+
+
+while True:
+    try:
+        num = int(input('Quantos Graus é o ângulo: ').strip())
+        cos_sen_tan(num)
+        break
+
+    except:
+        print('Valor Informado É Invalido')
